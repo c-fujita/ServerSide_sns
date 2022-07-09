adbc473a9fe80f9935798b0a581bb3d7ee7a4bf0
c-fujita <oor327@gmail.com>
2022-07-09 20:45:26 +0900

    create post table

---
+++ database/migrations/2022_07_09_082000_posts.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('user_name');
+
+            $table->string('contents', 100);
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->timestamp('update_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+        Schema::drop('posts');
+    }
+}
