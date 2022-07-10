3ff4ba8de09f50025ef62d7cb893dd45babad0a6
c-fujita <oor327@gmail.com>
2022-07-10 10:44:24 +0900

    edit html

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    
-    <header>
 
-        <h1></h1>
+    <header class="page-header">
+
+        <h1>みんなの掲示板</h1>
 
     </header>
 
     <div class="container">
 
-        <h2></h2>
+        <p><a class="btn btn-success" href="/create-form">投稿する</a></p>
+
+        <h2>みんなの投稿</h2>
 
         <table class="table table-hover">
 
@@ -36,5 +38,7 @@
             @endforeach
         </table>
     </div>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </body>
 </html>
